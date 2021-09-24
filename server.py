8d7e0a6414eb9bbf3d0a0ba9806d39e6e44f70c3
apurvasood99 <91176531+apurvasood99@users.noreply.github.com>
2021-09-24 11:01:02 +0530

    added some javascript and model

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
 from flask import Flask,render_template,request
+import pickle
+import json
+import numpy as np
+
 
 app = Flask(__name__)
 
@@ -13,6 +17,13 @@ def prediction():
     if request.method == 'POST':
       location = request.form.get('location')
       return render_template("prediction.html",locationStr=location)
+      
+      
+@app.route('/locations',methods=['get'])
+def locations():
+    with open("./model/locations.json", "r") as f:
+        __locations = json.load(f)['locations']
+    return __locations
     
 if __name__ == "__main__":
     app.run()
